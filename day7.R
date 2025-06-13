c21d4d4a3b27740a2ceeceadba1a5a89ff209a80
diwasbk <diwasbk903@gmail.com>
2025-06-13 22:17:05 +0545

    Bar Chart

--- day7.R
+++ day7.R
@@ -22,3 +22,26 @@ labels1 <- c("Nepal", "China", "UK", "Korea")
 pie3D(x = x1, labels = labels1, main = "Country Pie Chart", col = rainbow(length(x1)))
 legend("topright", labels1, cex = 0.5, fill = rainbow(length(x1)))
 
+
+# 2. BAR CHARTS
+# A bar chart is used to represent categorical data with rectangular bars.
+# The height of each bar is proportional to the value it represents.
+
+# Simple Bar Chart
+marks <- c(70, 85, 60, 95, 50, 80)
+subjects <- c("Physics", "Maths", "Nepali", "Computer", "Chemistry", "English")
+barplot(marks, names.arg = subjects, xlab = "Subjects", ylab = "Marks", 
+        main = "Marks Bar Chart", col = "yellow", border = "red")
+
+# Grouped Bar Chart (Multiple Bars)
+months <- c("Jan", "Feb", "March", "Apr", "May")
+regions <- c("Province 1", "Province 2", "Province 3")
+val <- matrix(c(21, 32, 33, 14, 95, 
+                46, 67, 78, 39, 11, 
+                22, 23, 94, 15, 26), 
+              nrow = 3, ncol = 5, byrow = TRUE)
+
+barplot(val, names.arg = months, xlab = "Months", ylab = "Revenue", 
+        main = "Total Revenue", col = rainbow(length(regions)))
+legend("topright", regions, cex = 0.8, fill = rainbow(length(regions)))
+
